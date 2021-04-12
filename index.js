928c81ad224296d562abb6718fba458a58c9ae7b
rangga124567 <54949636+rangga124567@users.noreply.github.com>
2021-04-13 01:00:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://vallak-team.glitch.me"]
+const urls = ["https://predator-.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
